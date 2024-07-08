8cacfdc8aefed1083238f6d31718062d5ef0538b
Null <kelanidarasimi9@gmail.com>
2024-07-08 20:21:18 +0100

    vercel json

--- app/db.py
+++ app/db.py
@@ -1,17 +1,17 @@
+import os
 from os import getenv
+from pathlib import Path
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from pathlib import Path
-import os
+
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent
-sslrootcert = BASE_DIR / 'ca.pem'
+sslrootcert = BASE_DIR / "ca.pem"
 
-DATABASE_URL = f"postgresql://{os.getenv('USERS')}:{os.getenv('PASSWORD')}@{os.getenv('HOST_NAME')}:{os.getenv('PORT')}/{os.getenv('DATABASE')}"
-print(DATABASE_URL)
+DATABASE_URL = f"postgresql://{os.getenv('USERS')}:{os.getenv('PASSWORD')}@{os.getenv('HOST_NAME')}:{int(os.getenv('PORT'))}/{os.getenv('DATABASE')}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
